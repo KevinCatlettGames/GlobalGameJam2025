8e6cb43d3fb77c0d987225b4ab0fc348581264f7
PongiPongi <sean.clever@web.de>
2025-05-03 22:20:16 +0200

    Materials and Models + Slip Rework

--- Assets/Scripts/General/DestroyAfterDuration.cs
+++ Assets/Scripts/General/DestroyAfterDuration.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class DestroyAfterDuration : MonoBehaviour
 {
     [SerializeField] private float waitDuration;
+    [SerializeField] private bool destroyOnRestart = true;
     
-    // Start is called before the first frame update
     void Start()
     {
+        if(destroyOnRestart) GameManager.Instance.OnGameStarted += DestroyOnRestart;
         Destroy(gameObject, waitDuration);
+    }
+
+    private void DestroyOnRestart()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if(destroyOnRestart) GameManager.Instance.OnGameStarted -= DestroyOnRestart;
     }
 }
