e244144e0bfc4ca1767066f2b14162fd3f905746
Nimmerschnell <captaintrasch@gmail.com>
2025-05-17 23:26:43 +0200

    push this

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using UnityEditor.Timeline;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
