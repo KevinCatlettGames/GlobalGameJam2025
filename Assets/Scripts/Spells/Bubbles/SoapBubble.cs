255d952592dd95e78cc83c6babe20eff64353170
Kevin Catlett <NebulaNookGames@gmail.com>
2025-05-26 13:04:04 +0200

    Revert "Fix: Effects no longer stay spawned and soap areas spawn correctly"
    This reverts commit d04fcbba67de0ad2874612a486d10109c2236429.

--- Assets/Scripts/Spells/Bubbles/SoapBubble.cs
+++ Assets/Scripts/Spells/Bubbles/SoapBubble.cs
@@ -25,6 +25,10 @@ public class SoapBubble : BasicBubble
                 GameObject puddle = Instantiate(soapPuddleObject, hitInfo.point, transform.rotation);
                 puddle.GetComponent<NetworkObject>()?.Spawn();
             }
+            else
+            {
+                Instantiate(soapPuddleObject, hitInfo.point, transform.rotation);
+            }
         }
     }
 
