5ed042161c2ecd5253756c3aa5d29b983f0cab54
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-05-29 23:40:43 +0200

    Floor death vfx

--- Assets/Scripts/Map/DeathzoneWall.cs
+++ Assets/Scripts/Map/DeathzoneWall.cs
@@ -4,14 +4,22 @@ public class DeathzoneWall : MonoBehaviour
 {
     [SerializeField] GameObject blastZoneEffect;
     private Vector3 effectDirection;
+    [SerializeField] private bool isFloor = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
         {
             Debug.Log("Death by Wall");
             Vector3 pos = other.transform.position;
-            effectDirection = other.GetComponent<CharacterController>().velocity * -1f;
-            Instantiate(blastZoneEffect, pos, Quaternion.LookRotation(effectDirection));
+            if (isFloor)
+            {
+                Instantiate(blastZoneEffect, pos, Quaternion.identity);
+            }
+            else
+            {
+                effectDirection = other.GetComponent<CharacterController>().velocity * -1f;
+                Instantiate(blastZoneEffect, pos, Quaternion.LookRotation(effectDirection));
+            }
             CameraShaker.instance.ShakeCamera(.3f, 20f);
         }
     }
