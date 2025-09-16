1387dd40113bf1f5879473741e6e6e98b99487dd
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-09-16 17:50:40 +0200

    Soap Droplet Map Event

---
+++ Assets/Scripts/Map/Map Events/SoapDroplet.cs
@@ -0,0 +1,77 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class SoapDroplet : NetworkBehaviour
+{
+    [SerializeField] private GameObject soapSplash;
+    [Header("Droplet Physics")]
+    [SerializeField] private Transform dropletTransform;
+    [SerializeField] private float startHeight = 50;
+    [SerializeField] private float dropletFallSpeed = 0f;
+    [SerializeField] private float gravity = 15;
+
+    [Header("Impact Damage")]
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float knockback = 5f;
+    [SerializeField] private float radius = 5f;
+
+    private bool hasExploded;
+
+    void Start()
+    {
+        dropletTransform.position = new Vector3 (dropletTransform.position.x, startHeight, dropletTransform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if (dropletTransform.position.y > 0)
+        {
+            dropletFallSpeed += Time.fixedDeltaTime * gravity;
+            dropletTransform.position = dropletTransform.position + Vector3.down * dropletFallSpeed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            GameObject splash = Instantiate(soapSplash, transform.position, Quaternion.identity);
+            splash.GetComponent<NetworkObject>()?.Spawn();
+
+            if (hasExploded) return;
+            hasExploded = true;
+            Collider[] explosionOverlaps = Physics.OverlapSphere(transform.position, radius, LayerMask.GetMask("Bubble", "Player"));
+            Vector3 origin;
+            Vector3 direction;
+            foreach (Collider col in explosionOverlaps)
+            {
+                if (col == null) continue;
+                origin = transform.position;
+                direction = col.transform.position - transform.position;
+                if (!Physics.Raycast(origin, direction, direction.magnitude, LayerMask.GetMask("Wall")))
+                {
+                    if (col.CompareTag("Player"))
+                    {
+                        PlayerController player = col.GetComponent<PlayerController>();
+                        if (player != null)
+                        {
+                            if (GameManager.Instance.PlayingLocal)
+                                player.ApplyKnockbackLocal(-1, direction, knockback, damage);
+                            else
+                                player.ApplyKnockbackServerRpc(-1, direction, knockback, damage);
+                        }
+                    }
+                    else
+                    {
+                        BasicBubble bubble = col.GetComponent<BasicBubble>();
+                        if (bubble != null)
+                        {
+                            bubble.BubbleCollision(this.gameObject);
+                        }
+                    }
+
+                }
+            }
+
+            NetworkObject.Despawn();
+            //Sound Here
+            Destroy(gameObject);
+        }
+    }
+}
