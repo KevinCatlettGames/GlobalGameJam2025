572a0132dcd4b41b45e411967fb3435df55b796d
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-11-16 16:25:37 +0100

    Ups

--- Assets/Scripts/Player/SkinSO.cs
+++ Assets/Scripts/Player/SkinSO.cs
@@ -7,12 +7,14 @@ public class SkinSO : ScriptableObject
     [FormerlySerializedAs("sprite")] [SerializeField] private Sprite gameSprite;
     [SerializeField] private Sprite lobbySprite;
     [SerializeField] private Color color;
+    [SerializeField] private int index; 
     [SerializeField] private GameObject skinPrefab;
     public Sprite GameSprite => gameSprite;
     
     public Sprite LobbySprite => lobbySprite;
     
     public Color Color => color;
+    public int Index => index;
 
     public GameObject SkinPrefab => skinPrefab;
 }
