ab4eaffc5b2be254a28ca919b9a075602d40d16b
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-11-18 14:58:07 +0100

    Portrait sprite switching

--- Assets/Scripts/Player/SkinSO.cs
+++ Assets/Scripts/Player/SkinSO.cs
@@ -4,16 +4,18 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "New SkinSO", menuName = "Scriptable Objects/SO_Skin")]
 public class SkinSO : ScriptableObject
 {
-    [FormerlySerializedAs("sprite")] [SerializeField] private Sprite gameSprite;
+    [FormerlySerializedAs("sprite")] 
+    [SerializeField] private Sprite[] gameSprites;
     [SerializeField] private Sprite lobbySprite;
     [SerializeField] private Color color;
     [SerializeField] private int index; 
     [SerializeField] private GameObject skinPrefab;
-    public Sprite GameSprite => gameSprite;
+    public Sprite[] GameSprites => gameSprites;
     
     public Sprite LobbySprite => lobbySprite;
     
     public Color Color => color;
+
     public int Index => index;
 
     public GameObject SkinPrefab => skinPrefab;
