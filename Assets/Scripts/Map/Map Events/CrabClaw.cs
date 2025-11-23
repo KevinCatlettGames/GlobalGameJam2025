51e092e36e889a29216de5675b5c145d2e91572a
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-11-23 19:22:01 +0100

    Arm Rotation

--- Assets/Scripts/Map/Map Events/CrabClaw.cs
+++ Assets/Scripts/Map/Map Events/CrabClaw.cs
@@ -8,6 +8,7 @@ public class CrabClaw : MonoBehaviour
     [SerializeField] private CrabHuntingGrounds huntingGrounds;
     [SerializeField] private LurkingShadow shadow;
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform clawTransform;
     [Header("Logic")]
     [SerializeField] private float startDelay = 8f;
     [SerializeField] private float restetTime = 5f;
@@ -83,7 +84,7 @@ public class CrabClaw : MonoBehaviour
                 yield return null;
             }
             //Change to anim event
-            transform.LookAt(Vector3.zero);
+            clawTransform.LookAt(Vector3.zero);
             animator.Play("Snap", 0, 0);
             Snap();
             yield return new WaitForSeconds(restetTime);
