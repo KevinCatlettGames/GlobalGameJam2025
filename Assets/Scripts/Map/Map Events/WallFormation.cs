647477fbb3144cee6ee459b71031b7e7ea52c8cf
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2025-12-22 22:17:45 +0100

    Wall Formations

---
+++ Assets/Scripts/Map/Map Events/WallFormation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class WallFormation : MonoBehaviour
+{
+    private RisingWall[] walls;
+    private bool isActive = false;
+    private void Awake()
+    {
+        walls = GetComponentsInChildren<RisingWall>(); 
+    }
+    public void RiseFormation()
+    {
+        if (isActive)
+            return;
+        isActive = true;
+        for (int i = 0; i < walls.Length; i++)
+        {
+            walls[i].gameObject.SetActive(true);
+            walls[i].Rise();
+        }
+    }
+    public void SinkFormation()
+    {
+        if (!isActive)
+            return;
+        isActive = false;
+        for (int i = 0; i < walls.Length; i++)
+        {
+            RisingWall wall = walls[i];
+            if (wall.gameObject.activeSelf && wall.IsActive)
+            {
+                walls[i].Sink(false);
+            }
+        }
+    }
+}
