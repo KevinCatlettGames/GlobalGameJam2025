796e670516f0a4ad4fda05cff35d50ade0593f48
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2026-06-01 22:46:20 +0200

    Random popup

--- Assets/Scripts/Player/DamagePopup.cs
+++ Assets/Scripts/Player/DamagePopup.cs
@@ -8,12 +8,18 @@ public class DamagePopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI damageText;
     [SerializeField] private float gradientEvaluateFactor = 0.005f;
     [SerializeField] private Gradient damageTextColorGradient;
+    [SerializeField] private float damageRandomOffset = .5f;
+    [SerializeField] private float yOffset = 1f;
 
     public void InitialiseDamagePopup(int damage)
     {
         float colorValue = (float)damage * gradientEvaluateFactor;
         damageText.color = damageTextColorGradient.Evaluate(colorValue);
         damageTypewriter.ShowText(damage.ToString());
+        Vector2 r = Random.insideUnitCircle;
+        Vector3 offset = new Vector3(r.x, yOffset, r.y) * damageRandomOffset;
+        transform.position += offset;
+
     }
 
     public void Dissapear()
