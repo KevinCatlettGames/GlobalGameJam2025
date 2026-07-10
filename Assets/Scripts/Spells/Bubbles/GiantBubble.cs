b5a8a0129b78ded3e515ebfe5302c71ed929c272
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2026-07-10 20:26:37 +0200

    Particle System Slots

--- Assets/Scripts/Spells/Bubbles/GiantBubble.cs
+++ Assets/Scripts/Spells/Bubbles/GiantBubble.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class GiantBubble : BasicBubble
 {
+    [Header("Particle Systems")]
+    [SerializeField] private ParticleSystem stationaryParticleSystem;
+    [SerializeField] private ParticleSystem bigParticleSystem;
+    [SerializeField] private ParticleSystem smallParticleSystem;
     [Header("Big Version")]
     [SerializeField] private float extraOffset = 2f;
     [SerializeField] private int knbDecreaseAngle = 45;
@@ -50,6 +54,8 @@ public class GiantBubble : BasicBubble
         InflateOverlapChack();
 
         sphereCollider.excludeLayers -= LayerMask.GetMask("Player");
+        stationaryParticleSystem.Stop();
+        bigParticleSystem.Play();
         hasInflated = true;
     }
 
@@ -82,6 +88,8 @@ public class GiantBubble : BasicBubble
             smallTrail.emitting = true;
             size *= sizMod;
             transform.localScale = Vector3.one * size;
+            bigParticleSystem.Stop();
+            smallParticleSystem.Play();
             return;
         }
         if (!isSmall && other.CompareTag("Player"))
