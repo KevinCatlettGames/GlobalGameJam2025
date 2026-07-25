c534c22c115faeb52dc31694fdd446596031dbd3
PongiPongi <157840908+PongiPongi@users.noreply.github.com>
2026-07-25 19:26:34 +0200

    Dynamic Zoom cam

--- Assets/Scripts/Cinematic/TargetGroupManager.cs
+++ Assets/Scripts/Cinematic/TargetGroupManager.cs
@@ -1,14 +1,33 @@
 using Cinemachine;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TargetGroupManager : MonoBehaviour
 {
-    private CinemachineTargetGroup targetGroup;
 
     public static TargetGroupManager Instance;
+    private List<Transform> targetGroup = new List<Transform> ();
 
-    [SerializeField] private float weight = 1f;
-    [SerializeField] private float radius = .5f;
+    [SerializeField] private float zoomSpeed = .3f;
+    [Header("Zoom Sizes")]
+    [SerializeField] private float maxSize = 21f;
+    [SerializeField] private float minSize = 16f;
+    [SerializeField] private float defaultSize = 19.75f;
+
+
+    [Header("Zoom Thresholds")]
+    [SerializeField] private float zoomInThreshold = 5f;
+    [SerializeField] private float zoomOutThreshold = 15f;
+
+    [SerializeField] private float minBound = 2.5f;
+    [SerializeField] private float maxBound = 20f;
+
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+    private float targetSize = 0f;
+    private float minStep = 0f;
+    private float maxStep = 0f;
 
     private void Awake()
     {
@@ -21,22 +40,75 @@ public class TargetGroupManager : MonoBehaviour
             Destroy(this);
         }
 
-        targetGroup = GetComponent<CinemachineTargetGroup>();
+
+        minStep = (defaultSize - minSize) / (zoomInThreshold - minBound);
+        maxStep = (maxSize - defaultSize) / (maxBound - zoomOutThreshold);
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnGameStarted += ResetZoom;
+    }
+
+    private void LateUpdate()
+    {
+        float boundsWidth = GetBoundsWidth();
+        if (boundsWidth == -1)
+        {
+            targetSize = defaultSize;
+        }
+        else if (boundsWidth > zoomOutThreshold)
+        {
+            if (boundsWidth > maxBound)
+                boundsWidth = maxBound;
+            targetSize = defaultSize + (boundsWidth - zoomOutThreshold) * maxStep;
+        }
+        else if (boundsWidth < zoomInThreshold)
+        {
+            if (boundsWidth < minBound)
+                boundsWidth = minBound;
+            targetSize = minSize + (boundsWidth - minBound) * minStep;
+        }
+        else
+        {
+           targetSize = defaultSize;
+        }
+
+        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, targetSize, Time.deltaTime * zoomSpeed);
+    }
+
+    public void ResetZoom()
+    {
+        virtualCamera.m_Lens.OrthographicSize = defaultSize;
+        targetSize = defaultSize;
+    }
+
+    private float GetBoundsWidth()
+    {
+        if (targetGroup.Count <= 1)
+            return -1;
+        Bounds bounds = new Bounds();
+        foreach (Transform t in targetGroup)
+        {
+            bounds.Encapsulate(t.transform.position);
+        }
+        return bounds.extents.x;
     }
 
     public void AddToGroup(Transform t)
     {
-        if (targetGroup.FindMember(t) != -1)
-            return;
-
-        targetGroup.AddMember(t, weight, radius);
+        if (!targetGroup.Contains(t))
+            targetGroup.Add(t);
     }
 
     public void RemoveFromGroup(Transform t)
     {
-        if (targetGroup.FindMember(t) == -1)
-            return;
+        if (targetGroup.Contains(t))
+            targetGroup.Remove(t);
+    }
 
-        targetGroup.RemoveMember(t);
+    private void OnDestroy()
+    {
+        GameManager.Instance.OnGameStarted -= ResetZoom;
     }
 }
