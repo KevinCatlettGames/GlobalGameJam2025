9eb16753eaef8fbddf725ef8cd1faef199016378
Kevin Catlett <kevin.catlett@terovania.de>
2026-09-22 03:26:50 +0200

    Dynamic kill count positioning in Scorescreen

--- Assets/Scripts/UI & Menus/UI/ScorePanelBackgroundFrameResizer.cs
+++ Assets/Scripts/UI & Menus/UI/ScorePanelBackgroundFrameResizer.cs
@@ -1,19 +1,36 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScorePanelBackgroundFrameResizer : MonoBehaviour
 {
     [SerializeField] private float[] frameXPositions;
     [SerializeField] private float[] frameWidths;
+    [SerializeField] private float[] killCounterPositions;
+
+    [Header("Kills Counter Target")]
+    [SerializeField] private RectTransform killsCounterRect;
+    private RectTransform myRectTransform;
+
+    private void Awake()
+    {
+        myRectTransform = GetComponent<RectTransform>();
+    }
 
     private void OnEnable()
     {
-
-        if (LobbyManager.instance && !LobbyManager.instance.playEndless.Value && LobbyManager.instance.winsNeeded.Value <= frameXPositions.Length)
+        if (LobbyManager.instance != null &&
+            !LobbyManager.instance.playEndless.Value &&
+            LobbyManager.instance.winsNeeded.Value > 0 &&
+            LobbyManager.instance.winsNeeded.Value <= frameXPositions.Length)
         {
-            GetComponent<RectTransform>().localPosition = new Vector3(frameXPositions[LobbyManager.instance.winsNeeded.Value - 1], transform.localPosition.y, transform.localPosition.z);
-            GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, frameWidths[LobbyManager.instance.winsNeeded.Value -1]);
+            int index = LobbyManager.instance.winsNeeded.Value - 1;
+
+            float targetX = frameXPositions[index];
+            float targetWidth = frameWidths[index];
+            float targetKillCounterXPos = killCounterPositions[index];
+            // 1. Resize and reposition background frame
+            myRectTransform.localPosition = new Vector3(targetX, myRectTransform.localPosition.y, myRectTransform.localPosition.z);
+            myRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, targetWidth);
+            killsCounterRect.localPosition = new Vector3(targetKillCounterXPos, killsCounterRect.localPosition.y, killsCounterRect.localPosition.z);
         }
     }
 }
